1ac697d01f0fd73989432d69d5224b01cede9a9b
TylerKimbell <tyler.kimbell@live.com>
2021-05-22 12:30:41 -0500

    Added Source Folder

--- ToDoList/HTM/HumanTaskManager.java
+++ ToDoList/HTM/HumanTaskManager.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 /* Human Task Manager 
  * Things to do:
- * Default Template And Delete Previous Saves Also Figure Out Why Template.txt isn't compiling and junk. 
+ * Perhaps create a default template if none exists.
  * The Ability To Create Categories. The Different categories will be categorized as either auto delete or manual delete 
  * Original Template That You Can Revert To If You Want to Reset (Literally Just a Text File) 
  *  	Such That There's A Reset Option That Just Reverts To The Default Template
--- ToDoList/HTM/PopUpMenu.java
+++ ToDoList/HTM/PopUpMenu.java
@@ -1,4 +1,3 @@
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
